7a8353b2dc421adb94ca69da3f43fdead23ef78e
avishekataur <avishekataur@gmail.com>
2021-10-25 21:00:07 +0600

    form-done

--- src/Components/AddReview/AddReview.js
+++ src/Components/AddReview/AddReview.js
@@ -1,11 +1,35 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const AddReview = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
-    <div>
-      <h3 className="text-center p-5">
+    <div className="mb-5 pb-3">
+      <h3 className="text-center mt-5 mb-5">
         OUR BELOVED <span className="text-success">CLIENTS</span>
       </h3>
+
+      <Form
+        className="d-flex flex-column w-25 mx-auto"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input className="mb-3 p-2" placeholder="Name" {...register("name")} />
+        <input
+          className="mb-3 p-2"
+          placeholder="Image URL"
+          {...register("img")}
+        />
+        <textarea
+          className="mb-3 p-2"
+          placeholder="Review"
+          {...register("review")}
+        />
+        <Button className="w-25" variant="success" type="submit">
+          Post
+        </Button>
+      </Form>
     </div>
   );
 };
