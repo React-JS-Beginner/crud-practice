a8064fcc94e71ae6eb9eeb45a8435ce95ed0ba8d
avishekataur <avishekataur@gmail.com>
2021-10-27 12:35:33 +0600

    delete-done-complete

--- src/Components/ManageReviews/ManageReviews.js
+++ src/Components/ManageReviews/ManageReviews.js
@@ -14,31 +14,32 @@ const ManageReviews = () => {
   );
 
   useEffect(() => {
-    fetch('http://localhost:5000/comments')
+    fetch("http://localhost:5000/comments")
       .then((res) => res.json())
       .then((data) => setComments(data));
     // .then((data) => console.log(data));
   }, []);
 
   //Delete
-  const deleteHandler = (id) => {   
-      fetch(`http://localhost:5000/comments/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          // console.log(data);
-          if (data.deleleCount > 0) {
-            const remainingComments = comments.filter(
-              (comment) => comment._id !== id
-            );
-            setComments(remainingComments);
-          }
-        });
-    
+  const deleteHandler = (id) => {
+    fetch(`http://localhost:5000/comments/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          const remainingComments = comments.filter(
+            (comment) => comment._id !== id
+
+          );
+        console.log(remainingComments);
+          setComments(remainingComments);
+        }
+      });
   };
 
   return (
@@ -47,50 +48,59 @@ const ManageReviews = () => {
         Manage <span className="text-success">reviews</span>
       </h3>
       <Container className="mb-5 pb-5 px-5">
-      
-      <Row className="g-5">
-        {
-
-        comments.map((comment) => (
-         <Col key={comment._id} comment={comment} className="d-flex align-items-stretch" xl={6} lg={6}>
-         <Card className="p-4 d-flex flex-row align-items-baseline">
-           
-             <div>
-             <div>
-               <span className="d-none">Hidden Text</span>
-               <br />
-               <img
-                 className="
+        <Row className="g-5">
+          {comments.map((comment) => (
+            <Col
+              key={comment._id}
+              comment={comment}
+              className="d-flex align-items-stretch"
+              xl={6}
+              lg={6}
+            >
+              <Card className="p-4 d-flex flex-row align-items-baseline">
+                <div>
+                  <div>
+                    <span className="d-none">Hidden Text</span>
+                    <br />
+                    <img
+                      className="
          rounded-circle
          border border-5 border-white
          shadow
          mb-3
          "
-                 style={{
-                   height: "60px",
-                   width: "60px",
-                 }}
-                 src={comment.profileImg}
-                 alt=""
-               />
-             </div>
-   
-             <div>
-               <p>
-                 <strong>{comment.profileName}</strong> <br />
-                 {comment.review}
-               </p>
-             </div>
-             </div>
+                      style={{
+                        height: "60px",
+                        width: "60px",
+                      }}
+                      src={comment.profileImg}
+                      alt=""
+                    />
+                  </div>
 
-           <Link onClick={()=>{deleteHandler(comment._id)}} to="#">{element}</Link>
-         </Card>
-       </Col>          
-        ))
+                  <div>
+                    <p>
+                      <strong>{comment.profileName}</strong> <br />
+                      {comment.review}
+                    </p>
+                  </div>
+                </div>
 
-        }
-      </Row>
-    </Container>
+                <Link
+                  onClick={() => {
+                    deleteHandler(comment._id);
+                  }}
+                  to="#"
+                >
+                  {element}
+                </Link>
+
+                {/* <button onClick={()=>{deleteHandler(comment._id)}} >{element}</button> */}
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
